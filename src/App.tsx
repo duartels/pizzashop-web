a067a6ce0956db1376696d401b2cc498c60015fc
Luan Duarte <luansouza789@gmail.com>
2024-01-29 17:56:39 -0300

    base configs for router, eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Button } from './components/ui/button'
 import './global.css'
 
-export function App() {
+import { RouterProvider } from 'react-router-dom'
 
-  return (
-    <div className='flex justify-center items-center h-screen'>
-      <Button>Button</Button>
-    </div>
-  )
+import { router } from './routes'
+
+export function App() {
+  return <RouterProvider router={router} />
 }
