001b02f253681fa5adfcdc3f3204051c04c46ba1
Luan Duarte <luansouza789@gmail.com>
2024-02-26 17:42:05 -0300

    dashboard metrics e2e added

---
+++ test/dashboard.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@playwright/test'
+
+test('display day orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('100').nth(1)).toBeVisible()
+  expect(page.getByText('+5% em relação a ontem')).toBeVisible()
+})
+
+test('display month orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('100').first()).toBeVisible()
+  expect(page.getByText('+5% em relação ao mês passado')).toBeVisible()
+})
+
+test('display month canceled orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('30')).toBeVisible()
+  expect(page.getByText('-5% em relação ao mês passado')).toBeVisible()
+})
+
+test('display month revenue metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('R$ 1.239,22')).toBeVisible()
+  expect(page.getByText('+22% em relação ao mês passado')).toBeVisible()
+})
